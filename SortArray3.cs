40c355c05298c04b6d48480734fc103b703dc260
mmxmm166 <41113067+mmxmm166@users.noreply.github.com>
2018-09-25 00:52:50 +0700

    Update SortArray3.cs

--- SortArray3.cs
+++ SortArray3.cs
@@ -6,35 +6,48 @@ namespace SortArray3
     {
         static void Main(string[] args)
         {
-          int[] My_Array = {1, 3, 4, 5, 6, 2, 7 };
-        //  int[] My_Array = {1,6,3,8,9, 4, 5, 6, 2, 7};
-        //    int[] My_Array = {3, 2, 5, 8};
+             int[] My_Array = {1, 3, 4, 5, 6, 2, 7 };
+          // int[] My_Array = {1, 6, 3, 8, 9, 4, 5, 6, 2, 7};
+          // int[] My_Array = {3, 2, 5, 8};
+          // int[] My_Array = {1, 2, 3 , 5, 4,8,6,7};
+            bool fl_sort = false;
 
-            bool fl = false;
             PrintArray(My_Array);
-            while (!fl )
+            while (!fl_sort)
             {
-                fl = true;
+                int k = 0;
+                fl_sort = true;
                 for (int i = 0; i < My_Array.Length - 1; i++)
                 {
-                    if (My_Array[i + 1] < My_Array[i])
+                    if (i < My_Array.Length - 2)
                     {
-                        if (i < 2)
-                        {
-                            //берем для сортировки три элемента начиная с i
-                            SortElements(My_Array, i);
-                        }
-                        else
-                        {
-                            //берем для сортировки три элемента начиная с i-1
-                            SortElements(My_Array, i - 1);
-                        }
-
-                        PrintArray(My_Array);
-                        fl = false;
+                        //берем для сортировки три элемента начиная с i
+                        k = i;
                     }
-                }
-            }
+                    else
+                    {
+                        //берем для сортировки три элемента начиная с i-1
+                        k = i - 1;
+                    }
+
+                    if ((My_Array[i + 1] < My_Array[i]) && (i < My_Array.Length - 2))
+                    {
+                        SortElements(My_Array, k);
+                        PrintArray(My_Array);
+                        fl_sort = false;
+                    }
+
+                    if ((i == My_Array.Length - 2) && (My_Array[i + 1] < My_Array[i]))
+                    {
+                        SortElements(My_Array, k);
+                        PrintArray(My_Array);
+                        if (fl_sort && !(My_Array[k] <= My_Array[k + 1] && My_Array[k + 1] <= My_Array[k + 2]))
+                        {
+                            Console.WriteLine("массив не сортируется");
+                        }
+                    }
+                 }
+            }  
             Console.ReadLine();
         }
  
